3ca7157b43ac01610506d38c094a46fdad4be5ca
Saugat Poudel <saugatpoudel2054@gmail.com>
2025-07-28 08:26:06 +0545

    allow turning around

--- main.py
+++ main.py
@@ -24,6 +24,7 @@ def main():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.update(dt)
         player.draw(screen)
         pygame.display.flip()
 
