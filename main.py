37a4dbfab34ff92803f0ea18be15290a284e6d8c
Saugat Poudel <saugatpoudel2054@gmail.com>
2025-07-28 08:18:25 +0545

    draw player

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ throughout this file"""
 import pygame
 
 from constants import *
+from player import Player
 
 def main():
     """ the main function of the program """
@@ -16,12 +17,14 @@ def main():
     print(f"Screen height: {SCREEN_HEIGHT}")
 
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         pygame.Surface.fill(screen, (0, 0, 0))
+        player.draw(screen)
         pygame.display.flip()
 
         dt = clock.tick(60)/1000
